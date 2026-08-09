e13c57fed5c6f29d50f5866fbc0ff24d3144ae8f
Minesh Varu <minesh.varu@gmail.com>
2026-08-09 17:19:57 -0400

    adding logic for cartSlice.js

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,26 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+        const {name, image, cost} = action.payload
+        const existing = state.items.find(g => action.payload.name === g.name)
 
+        if (!existing) {
+            state.items.quantity += 1
+        } else {
+            state.items.push({name, image, cost, quantity})
+        }
     },
     removeItem: (state, action) => {
+        const cartItems = items.filter(g => action.payload.name !== g.name)
+        state.items = cartItems
     },
     updateQuantity: (state, action) => {
+        const {name, quantity} = action.payload;
+        const item = state.items.find(g => g.name === name);
 
-    
+        if (item) {
+            item.quantity = quantity
+        }
     },
   },
 });
